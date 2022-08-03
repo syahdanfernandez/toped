e3063be2e69cb12d997ba96334ebee5c2ed85789
Muhamad Syahdan Fernandez <Syahdanfernandez@gmail.com>
2022-08-03 13:47:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="script.css">
+    <title>Bootstrap demo</title>
+    <script src="https://unpkg.com/feather-icons"></script>
+  </head>
+  <body>
+<div class="container">
+		<?php 
+		include 'navbar.php';
+		 ?>
+		<div class="container-fluid p-4">
+
+			<?php 
+			include 'carousel.php'
+			 ?>
+				
+		</div>
+</div>
+
+	<script>
+      feather.replace()
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Tokopedia</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item dropdown">
+	          <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            kategori
+	          </a>
+	          <ul class="dropdown-menu">
+	            <li><a class="dropdown-item" href="#">Action</a></li>
+	            <li><a class="dropdown-item" href="#">Another action</a></li>
+	            <li><hr class="dropdown-divider"></li>
+	            <li><a class="dropdown-item" href="#">Something else here</a></li>
+	          </ul>
+	        </li>   
+	    </ul>
+	    <form class="d-flex container-fluid" role="search">
+	        	<div class="input-group">
+	        		<input class="form-control" type="search" placeholder="Search" aria-label="Search">
+	        		<span class="input-group-text"><i data-feather="search"></i></span>
+	        	</div>
+	      	</form>	   
+	     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	    	<li class="nav-item dropdown me-2">
+	          <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            <i data-feather="shopping-cart"></i>
+	          </a>
+	          <ul class="dropdown-menu">
+	            <li><a class="dropdown-item" href="#">Action</a></li>
+	            <li><a class="dropdown-item" href="#">Another action</a></li>
+	            <li><hr class="dropdown-divider"></li>
+	            <li><a class="dropdown-item" href="#">Something else here</a></li>
+	          </ul>
+	        </li>
+		</ul>
+		<div class="me-3 " 	style="border-left:1px solid #000;height:30px">
+	        </div>
+
+	    	<div class="button-masuk">
+		        <button type="button" class="btn btn-outline-success me-3">Masuk</button>
+		    </div>
+		    <div class="button-daftar">
+		        <button type="button" class="btn btn-success ">Daftar</button>
+		    </div>    
+    </div>
+  </div>
+</nav>
